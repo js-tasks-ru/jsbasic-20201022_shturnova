36d48252941a8d52f9167096e79d4e8adb5bd103
eshturno <evgeniia.shturnova@t-systems.com>
2020-11-07 04:07:23 +0300

    задачи 1,2,4, проблемная 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -2,6 +2,21 @@
  * @param {string} str
  * @returns {string}
  */
-function camelize(str) {
-  // ваш код...
-}
+
+
+/*async function camelize(str) {
+
+  const  _ = await require('lodash.camelcase');
+
+  let result = _(str);
+  return Promise.resolve(result);
+
+} */
+//const  _ =  require('lodash.camelcase');
+import {camelCase} from 'lodash';
+
+ function camelize(str) {
+
+return camelCase(str);
+
+ }
