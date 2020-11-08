0cceb3b171f4ed66316e19f33d3e6f73a50727ca
eshturno <evgeniia.shturnova@t-systems.com>
2020-11-08 07:05:01 +0300

    4  модуль

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+
+  let ul = document.createElement("ul");
+  for(let element of friends) {
+    let li = document.createElement("li");
+    li.innerHTML = element.firstName + " " + element.lastName;
+    ul.appendChild(li);
+  }
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -2,5 +2,32 @@
  * Метод устанавливает необходимые по условию аттрибуты таблице
  * @param {Element} table
  */
+
 function highlight(table) {
-}
+  let rows = table.rows;
+  for (let i = 0; i < rows.length; i++) {
+
+
+    let status = rows[i].cells[3].dataset.available;
+    let gender = rows[i].cells[2].innerHTML;
+    let age = +rows[i].cells[1].innerHTML;
+
+    if (status === 'true') {
+      rows[i].className += ' available';
+    } else if (status === 'false') {
+      rows[i].className += ' unavailable';
+    } else if (status == undefined) {
+      rows[i].setAttribute('hidden', 'true');
+    }
+
+    if (gender === 'm') {
+      rows[i].className += ' male';
+    } else if (gender === 'f') {
+      rows[i].className += ' female';
+    }
+
+    if (age < 18) {
+      rows[i].setAttribute('style', 'text-decoration: line-through');
+    }
+  }
+};
